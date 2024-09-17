7dbfa4f849533c44df07b2c33cc5a2bd8b025028
Sagu39 <139549351+Sagu39@users.noreply.github.com>
2024-09-17 11:32:20 +0900

    Update AppException.java

--- src/main/java/com/example/demo/core/exception/AppException.java
+++ src/main/java/com/example/demo/core/exception/AppException.java
@@ -5,20 +5,20 @@ package com.example.demo.core.exception;
  * 業務エラーを検知した場合にスローする。
  * 基本的には再処理を促すメッセージを通知する。
  */
-public class AppException extends RunTimeException {
+public class AppException extends RuntimeException {
 
     /** メッセージID */
     private String messageId;
     /** フィールド */
     private String field;
-
+    
     /**
      * コンストラクタ
      * @param messageId メッセージID
      * @param field フィールド
      * @param cause 例外
      */
-    public AppException(String messageId, String code, Throwable cause) {
+    public AppException(String messageId, String field, Throwable cause) {
         super(cause);
         this.messageId = messageId;
         this.field = field;
